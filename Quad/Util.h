71b358bc05dba99b4f1a8b446388d2d000918d7d
jason-watkins <jason-watkins@outlook.com>
2015-02-06 17:19:39 -0800

    Added GPL license

--- Quad/Util.h
+++ Quad/Util.h
@@ -1,3 +1,20 @@
+// Copyright 2015 Jason Watkins
+// This file is part of the UCI UAVForge Quad-copter Controls System.
+//
+// UCI UAVForge Quad-copter Controls System is free software : you can
+// redistribute it and / or modify it under the terms of the GNU General Public
+// License as published by the Free Software Foundation, either version 3 of
+// the License, or (at your option) any later version.
+//
+// UCI UAVForge Quad-copter Controls System is distributed in the hope that it
+// will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// UCI UAVForge Quad-copter Controls System. If not, see
+// <http://www.gnu.org/licenses/>.
+
 #pragma once
 
 
