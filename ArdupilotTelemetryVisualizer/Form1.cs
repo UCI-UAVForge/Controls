7e99371277e3f02ff756e68cea00058bcbf02e51
jason-watkins <jason-watkins@outlook.com>
2015-02-13 12:00:32 -0800

    Added logging to CSV file

--- ArdupilotTelemetryVisualizer/Form1.cs
+++ ArdupilotTelemetryVisualizer/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.IO;
 
 namespace ArdupilotTelemetryVisualizer
 {
@@ -20,6 +21,7 @@ namespace ArdupilotTelemetryVisualizer
 		long[] lastPacket = new long[20];
 		int packetCur = 0;
 		DateTime lastTime;
+		StreamWriter sw;
 
 		public Form1()
 		{
@@ -28,6 +30,8 @@ namespace ArdupilotTelemetryVisualizer
 			sp.DataReceived += sp_DataReceived;
 			sp.Open();
 			lastTime = DateTime.Now;
+			FileStream fs = File.Open(DateTime.Now.ToString("yyyy-MM-dd mm.ss)") + ".log", FileMode.Create);
+			sw = new StreamWriter(fs, Encoding.UTF8);
 		}
 
 		void sp_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -42,10 +46,21 @@ namespace ArdupilotTelemetryVisualizer
 				{
 				this.Invoke(new Action(() =>
 					{
+						sw.Write(DateTime.Now);
+
 						ThrottleTextBox.Text = BitConverter.ToInt32(buffer, 0).ToString();
 						PitchTextBox.Text = BitConverter.ToSingle(buffer, 4).ToString();
 						RollTextBox.Text = BitConverter.ToSingle(buffer, 8).ToString();
 						YawTextBox.Text = BitConverter.ToSingle(buffer, 12).ToString();
+						sw.Write(ThrottleTextBox.Text);
+						sw.Write(',');
+						sw.Write(PitchTextBox.Text);
+						sw.Write(',');
+						sw.Write(RollTextBox.Text);
+						sw.Write(',');
+						sw.Write(YawTextBox.Text);
+						sw.Write(',');
+						
 
 						GyroPitchTextBox.Text = BitConverter.ToSingle(buffer, 16).ToString();
 						GyroRollTextBox.Text = BitConverter.ToSingle(buffer, 20).ToString();
@@ -54,28 +69,71 @@ namespace ArdupilotTelemetryVisualizer
 						OrientationPitchTextBox.Text = BitConverter.ToSingle(buffer, 32).ToString();
 						OrientationRollTextBox.Text = BitConverter.ToSingle(buffer, 36).ToString();
 						OrientationYawTextBox.Text = BitConverter.ToSingle(buffer, 40).ToString();
+						sw.Write(GyroPitchTextBox.Text);
+						sw.Write(',');
+						sw.Write(GyroRollTextBox.Text);
+						sw.Write(',');
+						sw.Write(GyroYawTextBox.Text);
+						sw.Write(',');
+						sw.Write(OrientationPitchTextBox.Text);
+						sw.Write(',');
+						sw.Write(OrientationRollTextBox.Text);
+						sw.Write(',');
+						sw.Write(OrientationYawTextBox.Text);
+						sw.Write(',');
 
 						StabPitchTextBox.Text = BitConverter.ToSingle(buffer, 44).ToString();
 						StabRollTextBox.Text = BitConverter.ToSingle(buffer, 48).ToString();
 						StabYawTextBox.Text = BitConverter.ToSingle(buffer, 52).ToString();
+						sw.Write(StabPitchTextBox.Text);
+						sw.Write(',');
+						sw.Write(StabRollTextBox.Text);
+						sw.Write(',');
+						sw.Write(StabYawTextBox.Text);
+						sw.Write(',');
 
 						RatePitchTextBox.Text = BitConverter.ToInt16(buffer, 56).ToString();
 						RateRollTextBox.Text = BitConverter.ToInt16(buffer, 60).ToString();
 						RateYawTextBox.Text = BitConverter.ToInt16(buffer, 64).ToString();
+						sw.Write(StabPitchTextBox.Text);
+						sw.Write(',');
+						sw.Write(StabRollTextBox.Text);
+						sw.Write(',');
+						sw.Write(StabYawTextBox.Text);
+						sw.Write(',');
 
 						FrontLeftTextBox.Text = BitConverter.ToUInt32(buffer, 68).ToString();
 						FrontRightTextBox.Text = BitConverter.ToUInt32(buffer, 72).ToString();
 						BackLeftTextBox.Text = BitConverter.ToUInt32(buffer, 76).ToString();
 						BackRightTextBox.Text = BitConverter.ToUInt32(buffer, 80).ToString();
+						sw.Write(FrontLeftTextBox.Text);
+						sw.Write(',');
+						sw.Write(FrontRightTextBox.Text);
+						sw.Write(',');
+						sw.Write(BackLeftTextBox.Text);
+						sw.Write(',');
+						sw.Write(BackRightTextBox.Text);
+						sw.Write(',');
 
 						RawThrottleTextBox.Text = BitConverter.ToUInt16(buffer, 88).ToString();
 						RawPitchTextBox.Text = BitConverter.ToUInt16(buffer, 86).ToString();
 						RawRollTextBox.Text = BitConverter.ToUInt16(buffer, 84).ToString();
 						RawYawTextBox.Text = BitConverter.ToUInt16(buffer, 90).ToString();
+						sw.Write(RawThrottleTextBox.Text);
+						sw.Write(',');
+						sw.Write(RawPitchTextBox.Text);
+						sw.Write(',');
+						sw.Write(RawRollTextBox.Text);
+						sw.Write(',');
+						sw.Write(RawYawTextBox.Text);
+						sw.Write(',');
 
 
 						MillisTextBox.Text = BitConverter.ToInt32(buffer, 92).ToString("#,###");
 						MicrosTextBox.Text = BitConverter.ToInt32(buffer, 96).ToString("#,###");
+						sw.Write(MicrosTextBox.Text);
+						sw.WriteLine();
+						sw.Flush();
 					}));
 				}
 				DateTime thisTime = DateTime.Now;
